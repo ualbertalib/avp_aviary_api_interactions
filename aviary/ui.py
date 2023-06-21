942207c74cf18512d6193534037997df0d05fbd4
Jeffery Antoniuk <jefferya@ualberta.ca>
2023-06-21 14:27:04 -0600

    Experimentation: downloading files * audio/video, transcripts, index, supplemental * Web UI login & MFA OTP

---
+++ aviary/ui.py
@@ -0,0 +1,89 @@
+# set of tools to interact with the Aviary UI
+
+import logging
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+
+
+def get_auth_from_file(args):
+    # copy the cookie from an authenticated Aviary Web UI session (via the browser dev tools -> network --> request --> cookie)
+    # write to a file (this will be the input file)
+    # todo: replace with a python auth script that can handle the MFA request
+    with open(args.session_cookie, 'r', encoding="utf-8", newline='') as input_file:
+        return input_file.read().rstrip("\n")
+
+# assume the auth cookie is captured from a browser session (kludge) and stored in a file
+# this is required to authenticate with the Web UI (different from the API)
+def build_session_header(args, token, url_path, session):
+    # get the x-csrf-token
+    headers = {"cookie": token}
+    response = session.get(
+        args.server + url_path,
+        headers=headers,
+    )
+    response.raise_for_status()
+    soup = BeautifulSoup(response.content, features="lxml")
+    csrf_token = soup.find('meta', {'name': 'csrf-token'})['content']
+    headers = {
+        "cookie": headers['cookie'],
+        "x-csrf-token": csrf_token
+    }
+    logging.info(headers)
+    return headers
+
+
+def init_session(args, username, password, otp_attempt):
+    session = requests.Session()
+    response = session.get(args.server)
+    logging.info(f"URL: {response.request.url}")
+    logging.info(f"Status: {response.status_code}")
+    #logging.info(f"Content: {response.content}")
+    logging.info(f"Headers: {response.headers}")
+    soup = BeautifulSoup(response.content, features="lxml")
+    authenticity_token = soup.find('input', {'name': 'authenticity_token'})['value']
+    data = {
+        'authenticity_token': authenticity_token,
+        'user[email]': username,
+        'user[password]': password,
+        'user[remember_me]': '0',
+        'site_url': args.server,
+        'commit': "Sign in"
+    }
+    url = urljoin(args.server, 'users/sign_in')
+    response = session.post(
+        url=urljoin(args.server, 'users/sign_in'),
+        data=data
+    )
+    logging.info(f"URL: {response.request.url}")
+    logging.info(f"Status: {response.status_code}")
+    #logging.info(f"Body: {response.request.body}")
+    #logging.info(f"Headers: {response.headers}")
+    #logging.info(f"Content: {response.content}")
+    # /html/body/div[2]/div[4]/div/main/div/div[3]/div/div[1]/div[2]/form/input
+    soup = BeautifulSoup(response.content, features="lxml")
+    authenticity_token_elements = soup.find_all('input', {'name': 'authenticity_token'})
+    for token_element in authenticity_token_elements:
+        logging.info(f"{token_element['value']}")
+    # find the authenticity_token in the correct form
+    # todo: add durability / error handling 
+    authenticity_token = soup.find_all('input', {'name': 'authenticity_token'})[2]['value']
+    data = {
+        'authenticity_token': authenticity_token,
+        'user[otp_attempt]': otp_attempt,
+        'commit': "Login"
+    }
+    response = session.post(
+        url=urljoin(args.server, 'users/sign_in'),
+        data=data
+    )
+    logging.info(f"URL: {response.request.url}")
+    logging.info(f"Status: {response.status_code}")
+    #logging.info(f"Body: {response.request.body}")
+    #logging.info(f"Headers: {response.headers}")
+    #logging.info(f"Content: {response.content}")
+
+    return session
+    
+
---
+++ experimental/experimental_download.py
@@ -0,0 +1,172 @@
+# Given a type and ID, download the attached file 
+
+# Proof-of-concept only
+
+from urllib.parse import urljoin
+from getpass import getpass
+from time import sleep
+import argparse
+from bs4 import BeautifulSoup
+import json
+import logging
+import os
+import requests
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from aviary import api as aviaryApi
+from aviary import utilities as aviaryUtilities
+from aviary import ui as aviaryUI
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server', required=True, help='Servername.')
+    parser.add_argument('--id', required=True, help='id.')
+    parser.add_argument('--type', required=True, help='One letter character for type [m]edia, [t]ranscript, [i]ndex, [s]upplemental file.')
+    parser.add_argument('--wait', required=False, help='Time to wait between API calls.', default=1.0)
+    parser.add_argument('--logging_level', required=False, help='Logging level.', default=logging.INFO)
+    parser.add_argument('--session_cookie', required=False, help='File containing an auth cookie from the WebUI (temporary).')
+    return parser.parse_args()
+
+
+def download_file(session, url, filename='tmp', path="/tmp/", headers=""):
+    logging.info(f"URL: {url}")
+    local_file_path = path + '/' + filename 
+    with session.get(url, stream=True, headers=headers) as response:
+        logging.info(f"Response: {response.request.url}")
+        response.raise_for_status()
+        with open(local_file_path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+    logging.info(f"Stored: {local_file_path}")
+
+    return local_file_path 
+
+def update_media_downloadable(session, server, id, value):
+    url = urljoin(server, 'api/v1/media_files/' + str(id))
+    value = 'true' if (value is True) else 'false'
+    params = {
+        #'is_downloadable': 'true' 
+        'is_downloadable': value
+    }
+    response = session.put(url, params=params)
+    logging.info(f"{response.request.url}")
+    logging.info(f"{response.content}")
+    return response.content
+
+def update_resource_access(session, server, id, value):
+    url = urljoin(server, 'api/v1/resources/' + str(id))
+    params = {
+        'access': value
+    }
+    response = session.put(url, params=params)
+    logging.info(f"{response.request.url}")
+    logging.info(f"{response.content}")
+    return response.content
+
+
+def download_media(session, args):
+    item = aviaryApi.get_media_item(args, session, args.id)
+    item_json = json.loads(item)
+    logging.info(f"Media: {item_json}")
+
+
+    # set resource private before enabling download link
+    # Todo: error check
+    #resource_id = item_json['data']['collection_resource_id'] 
+    #resource = aviaryApi.get_media_item(args, session, resource_id)
+    #resource_json = json.loads(item)
+    #original_resource_access = resource_json['data']['access']
+    #current_resource_access = resource_json['data']['access']
+    #if (original_resource_access != 'private'):
+    #    resource_json = update_resource_access(session, args.server, resource_id, 'private')
+    #    resource_json = json.loads(item)
+    #    current_resource_access = resource_json['data']['access']
+
+    # Need is_downloadable to be true to allow downloads
+    # Todo: error check
+    original_is_downloadable = item_json['data']['is_downloadable']
+    current_is_downloadable = False
+    if original_is_downloadable == False:
+        item = update_media_downloadable(session, args.server, args.id, True)
+        item_json = json.loads(item)
+        logging.info(f"Media: {item_json}")
+        current_is_downloadable = item_json['data']['is_downloadable']
+
+    url = item_json['data']['media_download_url']
+    filename = url.rsplit('/',1)[-1].split('?')[0]
+    download_file(session, url, filename)
+    
+    # Need is_downloadable to be set to the original value
+    if original_is_downloadable != current_is_downloadable: 
+        update_media_downloadable(session, args.server, args.id, original_is_downloadable)
+        item = aviaryApi.get_media_item(args, session, args.id)
+        item_json = json.loads(item)
+
+    # todo what if fails before? 
+    #if (original_resource_access != current_resource_access):
+    #    resource_json = update_resource_access(session, args.server, resource_id, original_resrouce_access)
+
+
+
+
+
+
+
+
+def process(args, session, headers=""):
+    if args.type == 's':
+        item = aviaryApi.get_supplemental_files_item(args, session, args.id)
+        item_json = json.loads(item)
+        logging.info(f"Supplemental File: {item_json}")
+        download_file(session, item_json['data']['file'], item_json['data']['associated_file_file_name'])
+    elif args.type == 't':
+        url = urljoin(args.server, '/transcripts/export/webvtt/' + str(args.id))
+        filename = args.id + '.webvtt'
+        download_file(session, url, filename, headers=headers)
+    elif args.type == 'i':
+        url = urljoin(args.server, '/indexes/export//' + str(args.id))
+        filename = args.id + '.webvtt'
+        download_file(session, url, filename, headers=headers)
+    elif args.type == 'm':
+        text = input("SANDBOX item only!!! Code modifies permissions. Continue (Y/n)?")
+        if (text == "Y"):
+            download_media(session, args)
+        
+#
+def main():
+    args = parse_args()
+
+    logging.getLogger().setLevel(args.logging_level)
+
+    username = input('Username:')
+    password = getpass('Password:')
+
+    if args.type == 't':
+        # todo: replace with a python auth script that can handle the MFA request
+        session_ui = requests.Session()
+        token = aviaryUI.get_auth_from_file(args)
+        headers = aviaryUI.build_session_header(args, token, '/transcripts', session_ui)
+        process(args, session_ui, headers)
+    elif args.type == 'i':
+        # todo: replace with a python auth script that can handle the MFA request
+        session_ui = requests.Session()
+        token = aviaryUI.get_auth_from_file(args)
+        headers = aviaryUI.build_session_header(args, token, '/indexes', session_ui)
+        process(args, session_ui, headers)
+    elif args.type == 'x':
+        # test Web UI auth
+        otp_attempt = getpass('OTP:')
+        session_ui = aviaryUI.init_session(args, username, password, otp_attempt)
+        url = urljoin(args.server, '/indexes/export//' + str(args.id))
+        filename = args.id + '.webvtt'
+        download_file(session_ui, url, filename)
+    else:
+        session = aviaryApi.init_session(args, username, password)
+        process(args, session)
+
+
+
+if __name__ == "__main__":
+    main()
